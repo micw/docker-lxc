272430b7137a0250620e9cf95f27d3e7fe056f6c
Andrew Grosser <dioptre@gmail.com>
2023-09-14 20:34:53 -0700

    working again

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#docker run --privileged  --hostname lxctest1 -v /tmp/lxc:/data -e USE_LXCFS=false -e DISTRIBUTION=alpine lxc
-docker run --privileged  --hostname lxctest1 -v /tmp:/data -e USE_LXCFS=false -e DISTRIBUTION=alpine -it lxc ash
+docker run --privileged  --hostname lxctest1 -v /tmp/lxc:/data -e USE_LXCFS=false -e DISTRIBUTION=alpine lxc
+#docker run --privileged  --hostname lxctest1 -v /tmp:/data -e USE_LXCFS=false -e DISTRIBUTION=alpine -it lxc ash
 #/usr/bin/python3 /scripts/launch.py
